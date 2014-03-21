a5f8b3486d70d32be9d8b4452704ddc1a89434bc
Michael Dresser <michaeldresser@darkminiserver.local>
2014-03-20 18:35:05 -0700

    Attempting to fix integration with another library

--- AGImagePickerController/AGIPCAssetsController.m
+++ AGImagePickerController/AGIPCAssetsController.m
@@ -139,7 +139,7 @@
         {
             UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
             space.width = 11;
-            self.navigationItem.rightBarButtonItems = @[space, doneButtonItem];
+            self.navigationItem.rightBarButtonItems = @[doneButtonItem, space];
         }
         else
         {
